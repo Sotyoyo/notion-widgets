e8697750f00cd3f987096d66c43e6489ae00ea3e
Sotyoyo <32472549+Sotyoyo@users.noreply.github.com>
2022-05-15 17:47:14 +0800

    fix

--- generates/wo-assets/axios.2c0f95a0.js
+++
@@ -1,1 +0,0 @@
-import{e,f as a}from"./index.ee629ba9.js";const i=a.create({baseURL:"https://api.example.com"});var t=e(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=i});export{i as api,t as default};
